d3d8f40cc58cd524b8feeeec9d60fb8cb27a442f
akbarahmedjonov <teamender1991@gmail.com>
2025-11-05 20:26:25 +0500

    commit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,3 @@
 require('config.options')
 require('config.keybinds')
 require('config.lazy')
-local pywal16 = require('pywal16')
-
-pywal16.setup()
-local lualine = require('lualine')
-
-lualine.setup {
-  options = {
-    theme = 'pywal16-nvim',
-  },
-}
